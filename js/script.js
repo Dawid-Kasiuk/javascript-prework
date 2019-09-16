1736a965968923a9dc063d4c77ad4aa8ebf506d7
Dawid Kasiuk <dawid_kasiuk@wp.pl>
2019-09-16 19:10:32 +0200

    Add first JS code

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let computerMove = `kamień`;
+let playComputer = `papier`;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagrałem ' + playComputer + '! Jeśli Twój ruch to nożyczki, to wygrywasz!')
